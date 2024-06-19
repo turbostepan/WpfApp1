42ff99e0f16dc4e720e2f72f15312ffce73e761b
Val200ya <australia636@gmail.com>
2024-06-19 11:56:30 +0300

    Добавьте файлы проекта.

---
+++ Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void But2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+
+        private void but4_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page3());
+        }
+    }
+}
---
+++ Page4.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+
+        private void but12_Click(object sender, RoutedEventArgs e)
+        
+            if (DataTable_dt_Table = MainWindow.Select("SELECT * FROM [dbo].[Table] WHERE [data] = '" + textBox_data.Text + "' AND [TableN] = '" + ComboBox_CB.Text)
+        {
+            MessageBox.Show("Столик успешно забронирован!");
+            NavigationService.Navigate(new Page1());
+        }
+    else MessageBox.Show("Упс! скорее всего столик уже заняли, попробуйте еще раз.");
+            NavigationService.Navigate(new Page1());
+
+             
+    }
+}
